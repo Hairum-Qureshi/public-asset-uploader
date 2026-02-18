a58a8d793b09a8f5fd6c18672e465319465d37a6
Hairum Qureshi <hqureshi@udel.edu>
2026-02-18 07:49:04 -0500

    added new route for uploading files

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
+import Uploader from "../pages/Uploader";
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/About" element={<About />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/upload/new-file" element={<Uploader />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
