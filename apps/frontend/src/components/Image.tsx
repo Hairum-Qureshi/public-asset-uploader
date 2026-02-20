076f4f46ad6a186a5b48b8a8040a43b1d2ca7c64
Hairum Qureshi <hqureshi@udel.edu>
2026-02-20 08:57:36 -0500

    added props to Image component and replaced hard coded placeholder data with dynamic prop data

--- apps/frontend/src/components/Image.tsx
+++ apps/frontend/src/components/Image.tsx
@@ -1,6 +1,20 @@
 import Actions from "./Actions";
 
-export default function Image() {
+interface ImageProps {
+	date: string;
+	size: number;
+	isUploader: boolean;
+	sourceURL: string;
+	name: string;
+}
+
+export default function Image({
+	date,
+	size,
+	isUploader,
+	sourceURL,
+	name
+}: ImageProps) {
 	return (
 		<tr className="bg-neutral-primary">
 			<th
@@ -8,20 +22,26 @@ export default function Image() {
 				className="px-6 py-4 font-medium text-heading whitespace-nowrap flex items-center"
 			>
 				<img
-					src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+					src={sourceURL}
 					alt="Image"
 					className="w-16 h-16 object-cover rounded mr-4"
 				/>
 				<div className="inline-block align-middle">
-					<p className="text-sm font-medium text-gray-900">Image.png</p>
+					<p className="text-sm font-medium text-gray-900">{name}</p>
 					<p className="text-sm text-gray-500">PNG Image</p>
 				</div>
 			</th>
 			<td className="px-6 py-4">
-				<p className="text-sm text-gray-500">2/20/2026</p>
+				<p className="text-sm text-gray-500">
+					{new Date(date).toLocaleDateString("en-US", {
+						year: "numeric",
+						month: "2-digit",
+						day: "2-digit"
+					})}
+				</p>
 			</td>
-			<td className="px-6 py-4">1.5 MB</td>
-			<Actions />
+			<td className="px-6 py-4">{size} MB</td>
+			<Actions isUploader />
 		</tr>
 	);
 }
