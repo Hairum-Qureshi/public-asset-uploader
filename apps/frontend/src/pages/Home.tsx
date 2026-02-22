85cad9a8c99e0366223f87d839914cd283adfced
Hairum Qureshi <hqureshi@udel.edu>
2026-02-22 01:06:24 -0500

    moved all table list view content to separate child ListView component to reduce Home component size; also added view logic and functions to buttons

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 import { GrStorage } from "react-icons/gr";
 import { IoGrid } from "react-icons/io5";
 import { FaList } from "react-icons/fa";
-import File from "../components/File";
-import Image from "../components/Image";
 import useFiles from "../hooks/useFiles";
+import ListView from "../components/views/ListView";
+import GridView from "../components/views/GridView";
 
 export default function Home() {
 	const [currSelected, setCurrSelected] = useState<
 		"all" | "images" | "videos" | "documents"
 	>("all");
+	const [galleryView, setGalleryView] = useState<"grid" | "list">("list");
 	const { allUploadedFiles, storageAmount, images, videos, documents } =
 		useFiles();
 
@@ -91,170 +92,30 @@ export default function Home() {
 						<button
 							className="flex items-center text-sky-500 hover:text-sky-700 hover:cursor-pointer text-xl"
 							title="Grid View"
+							onClick={() => setGalleryView("grid")}
 						>
 							<IoGrid />
 						</button>
 						<button
 							className="flex items-center text-sky-500 hover:text-sky-700 hover:cursor-pointer text-xl"
 							title="List View"
+							onClick={() => setGalleryView("list")}
 						>
 							<FaList />
 						</button>
 					</div>
 				</div>
-				<div>
-					<div>
-						<div className="relative overflow-x-auto">
-							{currSelected === "all" &&
-								(allUploadedFiles?.length ? (
-									<table className="w-full text-base text-left rtl:text-right text-body my-5">
-										<thead className="text-heading border-b border-gray-200">
-											<tr>
-												<th className="px-6 py-3 font-semibold">Name</th>
-												<th className="px-6 py-3 font-semibold">Date</th>
-												<th className="px-6 py-3 font-semibold">Size</th>
-												<th className="px-6 py-3 font-semibold">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											{allUploadedFiles.map(file => {
-												if (file.resource_type === "image") {
-													return (
-														<Image
-															key={file.asset_id}
-															name={file.display_name}
-															date={file.uploaded_at}
-															size={file.megaBytes}
-															sourceURL={file.secure_url}
-															isUploader
-														/>
-													);
-												}
-
-												if (file.resource_type === "video") {
-													return (
-														<Video
-															key={file.asset_id}
-															name={file.display_name}
-															date={file.uploaded_at}
-															size={file.megaBytes}
-															sourceURL={file.secure_url}
-															isUploader
-														/>
-													);
-												}
-
-												return (
-													<File
-														key={file.asset_id}
-														name={file.display_name}
-														date={file.uploaded_at}
-														size={file.megaBytes}
-														sourceURL={file.secure_url}
-														isUploader
-													/>
-												);
-											})}
-										</tbody>
-									</table>
-								) : (
-									<p className="text-center mt-10 text-gray-500">
-										No files uploaded yet.
-									</p>
-								))}
-
-							{currSelected === "images" &&
-								(images?.length ? (
-									<table className="w-full text-base text-left rtl:text-right text-body my-5">
-										<thead className="text-heading border-b border-gray-200">
-											<tr>
-												<th className="px-6 py-3 font-semibold">Name</th>
-												<th className="px-6 py-3 font-semibold">Date</th>
-												<th className="px-6 py-3 font-semibold">Size</th>
-												<th className="px-6 py-3 font-semibold">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											{images.map(image => (
-												<Image
-													key={image.asset_id}
-													name={image.display_name}
-													date={image.uploaded_at}
-													size={image.megaBytes}
-													sourceURL={image.secure_url}
-													isUploader
-												/>
-											))}
-										</tbody>
-									</table>
-								) : (
-									<p className="text-center mt-10 text-gray-500">
-										No images uploaded yet.
-									</p>
-								))}
-
-							{currSelected === "videos" &&
-								(videos?.length ? (
-									<table className="w-full text-base text-left rtl:text-right text-body my-5">
-										<thead className="text-heading border-b border-gray-200">
-											<tr>
-												<th className="px-6 py-3 font-semibold">Name</th>
-												<th className="px-6 py-3 font-semibold">Date</th>
-												<th className="px-6 py-3 font-semibold">Size</th>
-												<th className="px-6 py-3 font-semibold">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											{videos.map(video => (
-												<Video
-													key={video.asset_id}
-													name={video.display_name}
-													date={video.uploaded_at}
-													size={video.megaBytes}
-													sourceURL={video.secure_url}
-													isUploader
-												/>
-											))}
-										</tbody>
-									</table>
-								) : (
-									<p className="text-center mt-10 text-gray-500">
-										No videos uploaded yet.
-									</p>
-								))}
-
-							{currSelected === "documents" &&
-								(documents?.length ? (
-									<table className="w-full text-base text-left rtl:text-right text-body my-5">
-										<thead className="text-heading border-b border-gray-200">
-											<tr>
-												<th className="px-6 py-3 font-semibold">Name</th>
-												<th className="px-6 py-3 font-semibold">Date</th>
-												<th className="px-6 py-3 font-semibold">Size</th>
-												<th className="px-6 py-3 font-semibold">Actions</th>
-											</tr>
-										</thead>
-										<tbody>
-											{documents.map(document => (
-												<File
-													key={document.asset_id}
-													name={document.display_name}
-													date={document.uploaded_at}
-													size={document.megaBytes}
-													sourceURL={document.secure_url}
-													isUploader
-												/>
-											))}
-										</tbody>
-									</table>
-								) : (
-									<p className="text-center mt-10 text-gray-500">
-										No documents uploaded yet.
-									</p>
-								))}
-						</div>
-					</div>
-				</div>
+				{galleryView === "list" ? (
+					<ListView
+						currSelected={currSelected}
+						allUploadedFiles={allUploadedFiles}
+						images={images}
+						videos={videos}
+						documents={documents}
+					/>
+				) : (
+					<GridView />
+				)}
 			</div>
 		</div>
 	);
