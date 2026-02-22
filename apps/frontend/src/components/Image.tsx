f45309e18a1cf7ff3cb23c6a17bc9ff8322f2fb5
Hairum Qureshi <hqureshi@udel.edu>
2026-02-22 01:32:22 -0500

    modified UI depending on whether or not grid view is set or not

--- apps/frontend/src/components/Image.tsx
+++ apps/frontend/src/components/Image.tsx
@@ -1,47 +1,58 @@
+import type { FileProps } from "../interfaces";
 import Actions from "./Actions";
-
-interface ImageProps {
-	date: string;
-	size: number;
-	isUploader: boolean;
-	sourceURL: string;
-	name: string;
-}
 
 export default function Image({
 	date,
 	size,
 	isUploader,
 	sourceURL,
-	name
-}: ImageProps) {
+	name,
+	isGridView = false
+}: FileProps) {
 	return (
-		<tr className="bg-neutral-primary">
-			<th
-				scope="row"
-				className="px-6 py-4 font-medium text-heading whitespace-nowrap flex items-center"
-			>
-				<img
-					src={sourceURL}
-					alt="Image"
-					className="w-16 h-16 object-cover rounded mr-4"
-				/>
-				<div className="inline-block align-middle">
-					<p className="text-sm font-medium text-gray-900">{name}</p>
-					<p className="text-sm text-gray-500">PNG Image</p>
+		<>
+			{isGridView ? (
+				<div className="relative w-full h-full overflow-hidden rounded-md">
+					<img
+						src={sourceURL}
+						alt={name}
+						className="w-full h-full object-cover"
+					/>
+					{isUploader && (
+						<div className="absolute top-2 right-2">
+							<Actions isGridView isUploader={isUploader} />
+						</div>
+					)}
 				</div>
-			</th>
-			<td className="px-6 py-4">
-				<p className="text-sm text-gray-500">
-					{new Date(date).toLocaleDateString("en-US", {
-						year: "numeric",
-						month: "2-digit",
-						day: "2-digit"
-					})}
-				</p>
-			</td>
-			<td className="px-6 py-4">{size} MB</td>
-			<Actions isUploader />
-		</tr>
+			) : (
+				<tr className="bg-neutral-primary">
+					<th
+						scope="row"
+						className="px-6 py-4 font-medium text-heading whitespace-nowrap flex items-center"
+					>
+						<img
+							src={sourceURL}
+							alt="Image"
+							className="w-16 h-16 object-cover rounded mr-4"
+						/>
+						<div className="inline-block align-middle">
+							<p className="text-sm font-medium text-gray-900">{name}</p>
+							<p className="text-sm text-gray-500">PNG Image</p>
+						</div>
+					</th>
+					<td className="px-6 py-4">
+						<p className="text-sm text-gray-500">
+							{new Date(date).toLocaleDateString("en-US", {
+								year: "numeric",
+								month: "2-digit",
+								day: "2-digit"
+							})}
+						</p>
+					</td>
+					<td className="px-6 py-4">{size} MB</td>
+					<Actions isUploader />
+				</tr>
+			)}
+		</>
 	);
 }
