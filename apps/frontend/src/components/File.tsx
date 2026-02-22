8fd2deef99b71531ba6431e815947284186300fd
Hairum Qureshi <hqureshi@udel.edu>
2026-02-22 01:42:50 -0500

    passed new 'fileURL' prop to Actions component

--- apps/frontend/src/components/File.tsx
+++ apps/frontend/src/components/File.tsx
@@ -7,6 +7,7 @@ export default function File({
 	size,
 	isUploader,
 	name,
+	sourceURL,
 	isGridView = false
 }: FileProps) {
 	return (
@@ -19,7 +20,7 @@ export default function File({
 					</p>
 					{isUploader && (
 						<div className="absolute top-2 right-2">
-							<Actions isGridView isUploader={isUploader} />
+							<Actions isGridView isUploader fileURL={sourceURL} />
 						</div>
 					)}
 				</div>
@@ -45,7 +46,7 @@ export default function File({
 						</p>
 					</td>
 					<td className="px-6 py-4">{size} MB</td>
-					<Actions isUploader />
+					<Actions isUploader fileURL={sourceURL} />
 				</tr>
 			)}
 		</>
